d98530c9af32b091a340fee9d792accd84fadca3
Koen van Meijeren <52493874+KoenvanMeijeren@users.noreply.github.com>
2022-02-22 16:21:17 +0100

    Implement getting the game state

--- tests/Spec/game/GameDataSpec.js
+++ tests/Spec/game/GameDataSpec.js
@@ -6,4 +6,35 @@ describe('Game Data behavior tests', function () {
     it('should have an initialize method', function () {
         expect(typeof Game.Data.init).toBe("function")
     });
+
+    const onSuccess = jasmine.createSpy('onSuccess');
+    const onFailure = jasmine.createSpy('onFailure');
+
+    beforeEach(function () {
+        jasmine.Ajax.install();
+    });
+
+    afterEach(function () {
+        jasmine.Ajax.uninstall();
+    });
+
+    it("should return a game model", function () {
+        Env.init(Environments.development);
+        let promise = Game.Data.get();
+        return promise
+            .then(result => {
+                expect(result.Id).toBe(1);
+                expect(result.Description).toBe('This a Reversi game.');
+                expect(result.Token).toBe('fNtIKMuvJkSDBvuB8lbfCwii');
+
+                return onSuccess();
+            })
+            .catch(function (err) {
+                return onFailure();
+            })
+            .finally(function () {
+                expect(onSuccess).toHaveBeenCalled();
+                expect(onFailure).not.toHaveBeenCalled();
+            });
+    });
 });
---
+++ tests/Spec/system/EnvSpec.js
@@ -0,0 +1,27 @@
+describe('Env behavior', function () {
+
+    it('should have a global env module', function () {
+        expect(Env).toBeDefined();
+    });
+
+    it('should have an initialize method', function () {
+        expect(typeof Env.init).toBe("function")
+    });
+
+    it('should have a development env when requested', function () {
+        Env.init()
+        expect(Environments.development).toBe(Env.get());
+    });
+
+    it('should have a production env when requested', function () {
+        Env.init(Environments.production)
+        expect(Environments.production).toBe(Env.get());
+    });
+
+    it('should throw an error for an undefined environment', function () {
+        expect(function () {
+            Env.init('unknown env')
+        }).toThrow(new Error('Invalid environment given!'))
+    });
+
+});
