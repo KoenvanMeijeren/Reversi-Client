eab573e382436d121629d5ea0e201707df937265
Koen van Meijeren <52493874+KoenvanMeijeren@users.noreply.github.com>
2022-03-09 16:56:01 +0100

    Make the widget responsive

--- src/javascript/app.js
+++ src/javascript/app.js
@@ -4,7 +4,12 @@ $(document).ready(function () {
 
     const errorFeedBackWidget = new FeedbackWidget('feedback-danger');
     errorFeedBackWidget.addActions('Weigeren', 'Akkoord');
-    errorFeedBackWidget.show('Haha error!', FeedbackTypes.danger);
+    errorFeedBackWidget.show('Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia,\n' +
+        'molestiae quas vel sint commodi repudiandae consequuntur voluptatum laborum\n' +
+        'numquam blanditiis harum quisquam eius sed odit fugiat iusto fuga praesentium\n' +
+        'optio, eaque rerum! Provident similique accusantium nemo autem. Veritatis\n' +
+        'obcaecati tenetur iure eius earum ut molestias architecto voluptate aliquam\n' +
+        'nihil, eveniet aliquid culpa officia aut! Impedit sit sunt quaerat, odit.', FeedbackTypes.danger);
 
     Env.init(Environments.production);
     Game.init(afterGameInit);
