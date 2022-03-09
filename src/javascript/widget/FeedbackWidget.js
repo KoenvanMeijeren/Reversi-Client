31c6a8205c0af1b89f1f30b11015433b977f5fbc
Koen van Meijeren <52493874+KoenvanMeijeren@users.noreply.github.com>
2022-03-09 16:37:06 +0100

    Add icon to feedback widget

--- src/javascript/widget/FeedbackWidget.js
+++ src/javascript/widget/FeedbackWidget.js
@@ -42,7 +42,7 @@ class FeedbackWidget {
         if (element.length < 1) {
             $('body').append(`<div id='${elementId}' class='message-alert' role='alert'>
 <a href="#" class="button button-danger button-close" aria-label="Close"></a>
-<div class="message-container"><span class="icon"></span><div class="message"></div> </div>
+<div class="message-container"><div class="message"><span class="icon"></span></div> </div>
 </div>`);
         }
 
@@ -90,8 +90,8 @@ class FeedbackWidget {
         const element = this.#getElement();
 
         element.append(`<div class="actions">
-    <button type="button" class="button button-danger">${decline}</button>
-    <button type="button" class="button button-primary">${accept}</button>
+    <button type="button" class="button button-danger button-decline">${decline}</button>
+    <button type="button" class="button button-primary button-accept">${accept}</button>
 </div>`);
     }
 
@@ -107,7 +107,7 @@ class FeedbackWidget {
         this.log(message, type);
 
         const element = this.#getElement();
-        element.find('.message-container > .message').text(message);
+        element.find('.message-container > .message').append(message);
 
         element.removeClass(function (index, className) {
             return (className.match(/(^|\s)message-alert-\S+/g) || []).join(' ');
