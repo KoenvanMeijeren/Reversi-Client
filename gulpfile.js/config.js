069c940fba8d04ae941cb414d8ff25b19eb69d6b
Koen van Meijeren <52493874+KoenvanMeijeren@users.noreply.github.com>
2022-03-25 18:09:07 +0100

    Add reversi board

--- gulpfile.js/config.js
+++ gulpfile.js/config.js
@@ -16,17 +16,18 @@ module.exports = {
     },
   fileOrder: {
     js: [
-      'node_modules/jquery/dist/jquery.min.js',
-      'node_modules/bootstrap/dist/js/bootstrap.min.js',
-      'src/javascript/system/environment.js',
-      'src/javascript/widget/FeedbackWidget.js',
-      'src/javascript/game/model/GameModel.js',
-      'src/javascript/game/model/PlayerModel.js',
-      'src/javascript/game/game.js',
-      'src/javascript/game/game.reversi.js',
-      'src/javascript/game/game.model.js',
-      'src/javascript/game/game.data.js',
-      'src/javascript/app.js'
+        'node_modules/jquery/dist/jquery.min.js',
+        'node_modules/bootstrap/dist/js/bootstrap.min.js',
+        'src/javascript/system/environment.js',
+        'src/javascript/widget/FeedbackWidget.js',
+        'src/javascript/widget/GameBoardWidget.js',
+        'src/javascript/game/model/GameModel.js',
+        'src/javascript/game/model/PlayerModel.js',
+        'src/javascript/game/game.js',
+        'src/javascript/game/game.reversi.js',
+        'src/javascript/game/game.model.js',
+        'src/javascript/game/game.data.js',
+        'src/javascript/app.js'
     ],
     css: [
         'src/css/app.scss'
---
+++ src/javascript/widget/GameBoardWidget.js
@@ -0,0 +1,77 @@
+/**
+ * Provides widget for giving feedback.
+ */
+class GameBoardWidget {
+
+    /**
+     * The parent element.
+     *
+     * @type {jQuery}
+     */
+    #parent;
+
+    /**
+     * The game.
+     *
+     * @type {GameModel}
+     */
+    #game;
+
+    /**
+     * Constructs the game board widget.
+     *
+     * @param {jQuery} parent
+     *   The parent of the widget.
+     * @param {GameModel} game
+     *   The game.
+     */
+    constructor (parent, game) {
+        this.#parent = parent;
+        this.#game = game;
+    }
+
+    /**
+     * Renders the game.
+     */
+    render () {
+        this.#parent.html('');
+        this.#parent.append(this.#createTable());
+    }
+
+    /**
+     * Creates the table.
+     *
+     * @return {HTMLTableElement}
+     *   The renderable table.
+     */
+    #createTable () {
+        const table = document.createElement('table');
+        table.setAttribute('class', 'reversi-table');
+
+        this.#game.Board.forEach(function (boardRow) {
+            const row = document.createElement('tr');
+            row.setAttribute('class', 'reversi-row');
+            table.appendChild(row);
+
+            boardRow.forEach(function (boardColumn) {
+                const column = document.createElement('td');
+                column.setAttribute('class', `reversi-column`);
+
+                const fiche = document.createElement('div');
+                fiche.setAttribute('class', 'reversi-column-fiche');
+                fiche.setAttribute('data-selectable', boardColumn === Color.None ? 'true' : 'false');
+                fiche.setAttribute('data-color', boardColumn.toString());
+
+                column.appendChild(fiche);
+                row.appendChild(column);
+            });
+        });
+
+        return table;
+    }
+
+    #addRow () {
+
+    }
+
+}
