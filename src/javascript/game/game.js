e53db31708f28e239ec704d3b3a1dbee48113f10
Koen van Meijeren <52493874+KoenvanMeijeren@users.noreply.github.com>
2022-03-28 16:09:33 +0200

    Implement notifying the player on game ended and stop refreshing the game if so

--- src/javascript/game/game.js
+++ src/javascript/game/game.js
@@ -31,42 +31,37 @@ const Game = (function (url) {
         // Initializes the game.
         render();
 
-        // Refreshes the game state every 2 seconds.
-        setInterval(function () {
-            let game = get();
-            if (game.IsEnded()) {
+        Game.Data.get(Game.Data.getToken()).then(game => {
+            // Game has ended, so it is pointless to start refreshing the state.
+            const initGame = get();
+            if (initGame.IsEnded()) {
+                notifyPlayerIfRequested(initGame);
                 return;
             }
 
-            refreshGameState();
-            game = get();
-
-            // Notifies the player of changes, if requested.
-            const notifyPlayer = localStorage.getItem('notify-player');
-            if (notifyPlayer === 'true') {
-                if (game.Status === Status.Pending) {
-                    new FeedbackWidget(game.Status.toString()).show('Tegenstander gevonden!');
-                } else if (game.Status === Status.Playing) {
-                    new FeedbackWidget(game.Status.toString()).show('Reversi potje is gestart!');
-                } else if (game.Status === Status.Quit) {
-                    new FeedbackWidget(game.Status.toString()).show('Reversi potje is gestopt!');
-                } else if (game.Status === Status.Finished) {
-                    new FeedbackWidget(game.Status.toString()).show('Reversi potje is uitgespeeld!');
+            // Refreshes the game state every 2 seconds.
+            setInterval(function () {
+                let game = get();
+                if (game.IsEnded()) {
+                    if (localStorage.getItem('reload-game') === 'true') {
+                        location.reload();
+                    }
                 }
-            }
 
-            if (game?.CurrentPlayer.Token !== null && game?.CurrentPlayer.Token !== playerToken) {
-                console.log('Re-rendered the game');
+                refreshGameState();
+                game = get();
+
+                notifyPlayerIfRequested(game);
+
+                if (game?.CurrentPlayer.Token !== null && game?.CurrentPlayer.Token !== playerToken) {
+                    render();
+                }
+            }, config.refreshRate);
+
+            // Creates the event and listens to it for refreshing the game.
+            gameContainer.bind('refresh-reversi', function () {
                 render();
-                return;
-            }
-
-            console.log('Refreshed game state');
-        }, config.refreshRate);
-
-        // Creates the event and listens to it for refreshing the game.
-        gameContainer.bind('refresh-reversi', function () {
-            render();
+            });
         });
     };
 
@@ -79,6 +74,7 @@ const Game = (function (url) {
 
             localStorage.removeItem('notify-player');
             localStorage.setItem('notify-player', 'false');
+            localStorage.setItem('reload-game', 'false');
 
             // Initialize previous game state, if not present.
             if (stateMap.previousGame === null) {
@@ -89,6 +85,7 @@ const Game = (function (url) {
             if (game.Status !== stateMap.previousGame.Status) {
                 localStorage.removeItem('notify-player');
                 localStorage.setItem('notify-player', 'true');
+                localStorage.setItem('reload-game', 'true');
 
                 location.reload();
             }
@@ -109,6 +106,29 @@ const Game = (function (url) {
 
             Game.Reversi.initClickableFiches();
         });
+    };
+
+    /**
+     * Notifies the player of changes, if requested.
+     *
+     * @param {GameModel} game
+     *   The game.
+     */
+    const notifyPlayerIfRequested = function (game) {
+        const notifyPlayer = localStorage.getItem('notify-player');
+        if (notifyPlayer !== 'true') {
+            return;
+        }
+
+        if (game.Status === Status.Pending) {
+            new FeedbackWidget(game.Status.toString()).show('Tegenstander gevonden!');
+        } else if (game.Status === Status.Playing) {
+            new FeedbackWidget(game.Status.toString()).show('Reversi potje is gestart!');
+        } else if (game.Status === Status.Quit) {
+            new FeedbackWidget(game.Status.toString()).show('Reversi potje is gestopt!');
+        } else if (game.Status === Status.Finished) {
+            new FeedbackWidget(game.Status.toString()).show('Reversi potje is uitgespeeld!');
+        }
     };
 
     /**
